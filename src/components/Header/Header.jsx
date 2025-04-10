8af9f94f424c88f48284a9d4d1d82cfeedebdc88
umeraslam00 <joreypull@gmail.com>
2025-04-10 22:20:22 +0500

    setup components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import Container from '../container/Container'
 
 const Header = () => {
+
+  const authStatus = useSelector(state => state.auth.status)
+  //const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ]
+
+
+
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <div>abc</div>
+
+      </Container>
+    </header>
   )
 }
 
